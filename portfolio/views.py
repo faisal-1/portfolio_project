efa8bedb6c49bd840b5489db398433bcdc573b15
faisal-369 <hmfaisal.369@gmail.com>
2020-01-18 00:38:01 +0530

    first commit before about page

---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from . models import Projects
+# Create your views here.
+
+
+def home(request):
+    projects = Projects.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
+
+def about(request):
+    return render(request, 'portfolio/about.html')
